596954e4474cd4ac2dfb2fc0dcaf3a874c34278b
diegommarino <diegommarino@gmail.com>
2018-02-26 22:30:41 +0000

    [close #3]_welcome_page
    Some configurations for the tests needed to be made and the instalation of needed packages for the tests too.

---
+++ src/components/welcome/Welcome.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="welcome">
+    <h1>Welcome to Profile Matching</h1>
+    <p>Just check if your interests matches with another person interests.</p>
+    <div class="cta">
+      <a href="#">Sign Up</a>
+      <a href="#">Sign In</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  #welcome {
+    width: 80%;
+    margin: auto;
+  }
+
+  h1 {
+    text-align: center;
+  }
+
+  p {
+    text-align: center;
+  }
+
+  .cta {
+    width: 300px;
+    margin: auto;
+    text-align: center;
+  }
+
+  .cta a {
+    margin: 10px;
+    text-decoration: none;
+    display: inline-block;
+    border: 1px solid #521751;
+    border-radius: 3px;
+    width: 100px;
+    padding: 10px;
+    box-sizing: border-box;
+    color: #521751;
+  }
+
+  .cta a:hover,
+  .cta a:active {
+    background-color: #521751;
+    color: white;
+  }
+</style>
