9c855ef3b3ab5ab7c29d436b489087bc37a2e7b4
diegommarino <diegommarino@gmail.com>
2018-03-02 16:39:08 +0000

    [close #9]_profile_screen
    Still need to put the routes in the nav bar, but will do this when start to implement Axios, because the profile url will be dynamic.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Welcome from '@/components/welcome/Welcome'
 import ProfileForm from '@/components/profile/profileForm'
+import Profile from '@/components/profile/profile'
 import SignIn from '@/components/auth/signin'
 import SignUp from '@/components/auth/signup'
 
@@ -13,6 +14,7 @@ export default new Router({
   mode: 'history',
   routes: [
     { path: '/', name: 'Welcome', component: Welcome },
+    { path: '/profile', name: 'Profile', component: Profile },
     { path: '/profile-form', name: 'ProfileForm', component: ProfileForm },
     { path: '/signin', name: 'SignIn', component: SignIn },
     { path: '/signup', name: 'SignUp', component: SignUp }
